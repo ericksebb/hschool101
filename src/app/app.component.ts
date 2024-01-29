4336c460aa288181f11d8691ef6a796082a4666e
Erick Sebastián <esprado@sudamericano.edu.ec>
2024-01-29 18:36:32 -0500

    post revision

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LandingComponent } from './landing/landing.component';
+import { BlogpostComponent } from './blogpost/blogpost.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LandingComponent, BlogpostComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/blogpost/blogpost.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-blogpost',
+  standalone: true,
+  imports: [],
+  templateUrl: './blogpost.component.html',
+  styleUrl: './blogpost.component.css'
+})
+export class BlogpostComponent {
+
+}
